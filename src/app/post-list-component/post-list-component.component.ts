9d1dd552ae0812ca29360e877799b0cee0e62a8a
Kaique-GM <kaiquegmoficial@gmail.com>
2025-03-08 15:50:10 -0300

    Header

---
+++ src/app/post-list-component/post-list-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-component',
+  imports: [],
+  templateUrl: './post-list-component.component.html',
+  styleUrl: './post-list-component.component.css'
+})
+export class PostListComponentComponent {
+
+}
